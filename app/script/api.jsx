66d832938185e04b92dd607d6a18ed6914abc10b
asami.otsu <asami.otsu@nijibox.co.jp>
2019-03-27 15:03:33 +0900

    型変更

--- app/script/api.jsx
+++ app/script/api.jsx
@@ -6,7 +6,7 @@ const PROJECT_COLOR_MAX = 12;
 
 let path = {};
 let userId = '';
-let assignedProjects = '';
+let assignedProjects = [];
 
 export default class Api {
     constructor(url, params) {
